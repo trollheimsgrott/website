966e967d14ca40a2ab8fe5666a5340218950bbdf
trollheimsgrott <finlay.macbeth@gmail.com>
2022-03-02 12:56:25 +1300

    Add some stuff.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Website
+
+This is an example website made with HTML, CSS and Javascript.
---
+++ assets/application.js
@@ -0,0 +1,8 @@
+function doIt() {
+    console.log("Hello, World!");
+    var name = prompt("What's your name?");
+    alert("Hello, " + name);
+}
+
+
+doIt();
